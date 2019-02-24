a221bb72b95ab426f4b1779cfb6d03cf7168af11
Max Rittmüller <max.rittmueller@gmail.com>
2019-02-24 01:47:09 +0100

    only run prettier on ts files

--- src/context/defaultStores/index.ts
+++ src/context/defaultStores/index.ts
@@ -2,9 +2,9 @@ import game, { gameStoreProps } from './gameStore';
 import { defaultPersistedStore } from '../persist';
 
 export interface globalStore {
-  game: gameStoreProps,
-  persisted: any
-} 
+  game: gameStoreProps;
+  persisted: any;
+}
 
 export default {
   game,
